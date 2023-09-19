591a14093dad1b7a88b52d054ba0d87aa873659b
Kristin Valentine <kristinw@gmail.com>
2023-09-19 14:01:01 -0700

    install vitest and react testing lib; set up example

---
+++ src/__tests__/example.jsx
@@ -0,0 +1,9 @@
+// import { expect, test } from 'vitest';
+// import { render } from '@testing-library/react';
+// import Component from '../components/Component'
+
+// test("does a very important thing", async () => {
+//     const component = render(<Component/>);
+//     expect(component).toContain('stuff');
+//     component.unmount();
+// });
